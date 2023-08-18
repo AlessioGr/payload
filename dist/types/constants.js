8722135e9c21874b424a56c7113cd744928faa1a
Alessio Gravili <alessio@gravili.de>
2023-08-18 19:51:58 +0200

    commit dist folder

---
+++ dist/types/constants.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.validOperators = void 0;
+exports.validOperators = [
+    'equals',
+    'contains',
+    'not_equals',
+    'in',
+    'all',
+    'not_in',
+    'exists',
+    'greater_than',
+    'greater_than_equal',
+    'less_than',
+    'less_than_equal',
+    'like',
+    'near',
+];
+//# sourceMappingURL=constants.js.map
